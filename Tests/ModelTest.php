cc405245918912339205bb1498dda555edc65146
Ross Riley <riley.ross@gmail.com>
2013-12-16 11:38:14 +0000

    added test for column alterations

--- Tests/ModelTest.php
+++ Tests/ModelTest.php
@@ -31,6 +31,21 @@ class ModelTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($res, null);
   }
 
+  public function test_schema_alterations() {
+    $model = new MockModel($this->db);
+    $result = $model->insert(["title"=>"Hello World"]);
+    $this->assertEquals($result, 1);
+
+    $model->freeze = true;
+    $this->setExpectedException('Doctrine\DBAL\DBALException');
+    $model->define("newcolumn", "string");
+    $result2 = $model->update(1,["newcolumn"=>"testing"]);
+
+    $model->freeze = false;
+    $result3 = $model->update(1,["newcolumn"=>"testing"]);
+    $this->assertEquals($result3, 1);
+  }
+
   public function test_insert_and_find() {
     $model = new MockModel($this->db);
     $result = $model->insert(["title"=>"Hello World"]);
@@ -38,6 +53,7 @@ class ModelTest extends \PHPUnit_Framework_TestCase {
     $model2 = new MockModel($this->db);
     $res = $model2->find(1);
     $this->assertEquals($res["title"], "Hello World");
+    $model2->update(1,["title"=>"Goodbye World"]);
   }
 
   public function test_update() {
