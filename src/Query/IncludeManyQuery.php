f1ce19477f3f450daae969a517b475ac9f7075ff
Ross Riley <riley.ross@gmail.com>
2014-02-13 16:25:17 +0000

    PSR2 standards

--- src/Query/IncludeManyQuery.php
+++ src/Query/IncludeManyQuery.php
@@ -7,36 +7,43 @@ namespace SlimModel\Query;
  * from a many to many join.
  *
  */
-class IncludeManyQuery {
+class IncludeManyQuery
+{
 
-  public $db;
-  public $options;
-  public $key_filter;
-  public $query;
-
-  public function __construct($db = false, $include_options = [], $key_filter = []) {
-    $this->db = $db;
-    $this->options = $include_options;
-    $this->key_filter = $key_filter;
-  }
+    public $db;
+    public $options;
+    public $key_filter;
+    public $query;
 
 
-  public function build() {
-    $this->query = $this->db->createQueryBuilder();
-    $this->query->select("l.id as lkey, r.*")
-       ->from($this->options["origin"],"l")
-       ->leftjoin("l", $this->options["join"], "j", "j.{$this->options['join_left_key']} = l.{$this->options['key']}")
-       ->leftjoin("l", $this->options["table"],"r", "r.{$this->options['join_key']} = j.{$this->options['join_right_key']}")
-       ->where("r.{$this->options['join_key']} IS NOT NULL");
-    if(count($this->key_filter)) {
-      $this->query->andwhere($this->query->expr()->in("l.{$this->options['key']}", $this->key_filter));
+    public function __construct($db = false, $include_options = [], $key_filter = [])
+    {
+        $this->db = $db;
+        $this->options = $include_options;
+        $this->key_filter = $key_filter;
     }
-  }
 
-  public function execute() {
-    $this->build();
-    return $this->query->execute()->fetchAll();
-  }
+
+    public function build()
+    {
+        $this->query = $this->db->createQueryBuilder();
+        $this->query->select("l.id as lkey, r.*")
+            ->from($this->options["origin"],"l")
+            ->leftjoin("l", $this->options["join"], "j", "j.{$this->options['join_left_key']} = l.{$this->options['key']}")
+            ->leftjoin("l", $this->options["table"],"r", "r.{$this->options['join_key']} = j.{$this->options['join_right_key']}")
+            ->where("r.{$this->options['join_key']} IS NOT NULL");
+
+        if (count($this->key_filter)) {
+            $this->query->andwhere($this->query->expr()->in("l.{$this->options['key']}", $this->key_filter));
+        }
+
+    }
+
+    public function execute()
+    {
+        $this->build();
+        return $this->query->execute()->fetchAll();
+    }
 
 
 
