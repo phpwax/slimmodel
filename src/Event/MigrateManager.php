11d12ed41cfad8f9975954d622ae5023a34ded9f
Ross Riley <riley.ross@gmail.com>
2014-02-13 16:19:55 +0000

    PSR2 standards

--- src/Event/MigrateManager.php
+++ src/Event/MigrateManager.php
@@ -13,50 +13,59 @@ use Doctrine\Common\EventSubscriber;
  */
 class MigrateManager implements EventSubscriber {
 
-  protected $model;
-  protected $event_data;
+    protected $model;
+    protected $event_data;
 
 
-  public function __construct($model = false) {
-    $this->model = $model;
-  }
-
-  public function getSubscribedEvents() {
-    return [
-      "onSchemaException"
-    ];
-  }
-
-  public function onSchemaException(ModelEventArgs $event) {
-    if($event->model->freeze) return true;
-    else $this->migrate($event->model);
-  }
-
-  public function migrate($model) {
-    /* Initialise the schema variables from the connection */
-    $platform = $model->db->getDatabasePlatform();
-    $sm = $model->db->getSchemaManager();
-    $original_schema = $sm->createSchema();
-    $schema = new Schema();
-
-
-    /* Now use the Schema object to create a table */
-    if(!$schema->hasTable($model->table)) $table = $schema->createTable($model->table);
-    else $table = $schema->getTable($model->table);
-
-    foreach($model->columns as $name=>$options) {
-      $table->addColumn($name,   $options["type"],  $options["options"]);
+    public function __construct($model = false)
+    {
+        $this->model = $model;
     }
-    $table->setPrimaryKey(array($model->primary_key));
 
-    /* We now have a current Schema object, which is compared against the original to create migration sql */
-    $queries = $schema->getMigrateFromSql($original_schema, $platform);
 
-    /* It produces a series of queries, which we run in sequence. The database should be up to date now. */
-    foreach ($queries as $query) {
-      $model->db->query($query);
+    public function getSubscribedEvents()
+    {
+        return [
+            "onSchemaException"
+        ];
     }
-  }
+
+
+    public function onSchemaException(ModelEventArgs $event)
+    {
+        if($event->model->freeze) return true;
+        else $this->migrate($event->model);
+    }
+
+
+    public function migrate($model)
+    {
+
+        /* Initialise the schema variables from the connection */
+        $platform = $model->db->getDatabasePlatform();
+        $sm = $model->db->getSchemaManager();
+        $original_schema = $sm->createSchema();
+        $schema = new Schema();
+
+
+        /* Now use the Schema object to create a table */
+        if(!$schema->hasTable($model->table)) $table = $schema->createTable($model->table);
+        else $table = $schema->getTable($model->table);
+
+        foreach($model->columns as $name=>$options) {
+            $table->addColumn($name,   $options["type"],  $options["options"]);
+        }
+
+        $table->setPrimaryKey(array($model->primary_key));
+
+        /* We now have a current Schema object, which is compared against the original to create migration sql */
+        $queries = $schema->getMigrateFromSql($original_schema, $platform);
+
+        /* It produces a series of queries, which we run in sequence. The database should be up to date now. */
+        foreach ($queries as $query) {
+            $model->db->query($query);
+        }
+    }
 
 
 }
